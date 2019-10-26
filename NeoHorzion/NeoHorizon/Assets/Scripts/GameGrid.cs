0baba7a77ddafa8e24363ceeb8306baf9eb1eea2
Jarrett Ang <jarrett_ang@hotmail.com>
2019-10-26 16:45:11 +0900

    Added basic grid

---
+++ NeoHorzion/NeoHorizon/Assets/Scripts/GameGrid.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGrid : MonoBehaviour
+{
+    [Header("Attributes")]
+    [SerializeField] private int width = 8;
+    [SerializeField] private int height = 25;
+    [SerializeField] private float cellSize = 5;
+
+
+    private GameGridCell[,] grid;
+
+    private void Awake() {
+        grid = new GameGridCell[width, height];
+
+        Vector2 topLeftPos;
+
+        // Start spawning from top left
+        if(width % 2 == 0) {
+            topLeftPos.x = (cellSize * (float)(-width / 2)) + (cellSize / 2f);
+        } else {
+            topLeftPos.x = cellSize * Mathf.Floor(-width / 2);
+        }
+        if(height % 2 == 0) {
+            topLeftPos.y = (cellSize * (float)(-height / 2)) + (cellSize / 2f);
+        } else {
+            topLeftPos.y = cellSize * Mathf.Floor(-height / 2);
+        }
+
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+                Vector2 spawnPos = new Vector2(topLeftPos.x + cellSize * x, topLeftPos.y + cellSize * y);
+
+                GameGridCell newCell = new GameGridCell(spawnPos);
+            }
+        }
+    }
+}
---
+++ NeoHorzion/NeoHorizon/Assets/Scripts/GameGridCell.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGridCell
+{
+    public GameGridCell(Vector2 spawnPos) {
+        position = spawnPos;
+
+        statusSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        statusSphere.transform.position = position;
+
+        DebugStatus();
+    }
+
+    public TilePiece CurrentPiece;
+
+    private Vector2 position;
+
+    private GameObject statusSphere;
+
+    public void DebugStatus() {
+        if(CurrentPiece == null) {
+            statusSphere.GetComponent<Renderer>().material.color = Color.red;
+        } else {
+            statusSphere.GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+}
