024bb438722fdbc575ef8173a396fc5203e6a782
Jarrett Ang <jarrett_ang@hotmail.com>
2019-10-28 18:41:12 +0900

    Added game over and mid-game quit

--- NeoHorzion/NeoHorizon/Assets/Scripts/_Managers/GameOverManager.cs
+++ NeoHorzion/NeoHorizon/Assets/Scripts/_Managers/GameOverManager.cs
@@ -1,20 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameOverManager : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private GameObject gameOverPanel;
+
+    [Header("Attributes")]
+    [SerializeField, Tooltip("Axes for selecting")] private string quitButton = "Cancel";
+    [SerializeField, Tooltip("Axes for selecting")] private string retryButton = "Shoot";
+    [SerializeField] private float gameOverWaitSeconds = 1f;
+
+    private bool gameOver = false;
+
+    private bool gameOverPressedOnce = false;
+
     private void Awake() {
         EventManager.OnGameOver += ShowGameOver;
+    }
+
+    private void Update() {
+        if(!gameOver) {
+            if(Input.GetButtonDown(quitButton)) {
+                if(gameOverPressedOnce) {
+                    SceneSwapper.Instance.LoadStartScene();
+                } else {
+                    gameOverPressedOnce = true;
+                    Invoke("ResetPressedOnce", gameOverWaitSeconds);
+                }
+            }
+        } else {
+            if(Input.GetButtonDown(retryButton)) {
+                SceneSwapper.Instance.ReloadCurrentScene();
+                Time.timeScale = 1;
+            } else if(Input.GetButtonDown(quitButton)) {
+                SceneSwapper.Instance.LoadStartScene();
+                Time.timeScale = 1;
+            }
+        }
     }
 
     private void ShowGameOver() {
         SoundManager.Instance.PlaySound("GameOver");
 
-
-        Debug.Log("GAME OVER!!!!!!!!!!!!!!!!!!!!");
-
         Time.timeScale = 0;
+
+        gameOverPanel.SetActive(true);
+        gameOver = true;
+    }
+
+    private void ResetPressedOnce() {
+        gameOverPressedOnce = false;
     }
 }
