c1576664660f2db0c582e90c39ea501ba15d8adb
Jarrett Ang <jarrett_ang@hotmail.com>
2019-10-28 19:12:56 +0900

    Added ball basic

---
+++ NeoHorzion/NeoHorizon/Assets/Scripts/Entities/Player/BallShooter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallShooter : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform[] shootLines;
+    [SerializeField] private SpriteRenderer[] shootLinesImage;
+
+    [Header("Attributes")]
+    [SerializeField] private float swapTime = 0.75f;
+
+    private WaitForSeconds waitTime;
+
+    private void Awake() {
+        waitTime = new WaitForSeconds(swapTime);
+    }
+
+    private void Start() {
+        StartCoroutine(ShootLineSwapping());
+    }
+
+    private IEnumerator ShootLineSwapping() {
+        //while(true) {
+            
+        //}
+    }
+}
